f7a52fab7aa2a479dc9510fb479d18bb41262850
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-08-21 12:52:10 +1000

    formated the according to pep8 standards

--- demo_meta_miner/execute_saved_req.py
+++ demo_meta_miner/execute_saved_req.py
@@ -2,23 +2,47 @@ import json
 import utils as utils
 import click
 
-@click.command()
-@click.option('--auth', default='910923131171f6c4ae9bd84cbb5d5d44edb14436', help='Authentication token')
 
-def execute_migration(auth):
-    """This script consumes the data.json file to upload the metadata to Aristotle"""
+@click.command()
+@click.option(
+    '--auth',
+    default='910923131171f6c4ae9bd84cbb5d5d44edb14436',
+    help='Authentication token'
+    )
+@click.option(
+    '--file',
+    default='data.json',
+    help='Spicify the json file name'
+    )
+def execute_migration(auth, file):
+    """This script consumes the json file to upload the metadata to Aristotle"""
     req_data = []
-    with open('data.json', 'r') as fd:
+    with open(file, 'r') as fd:
         req_data = json.load(fd)
 
     for distribution in req_data:
-        for i,data_element in enumerate(distribution['fields']['data_elements']):
-            valueDomain = utils.request_get(auth=auth,model="valuedomain",name= data_element['data_element']['fields']['valueDomain']['fields']['name'],app="aristotle_mdr")
+        for i, data_element in enumerate(distribution['fields']['data_elements']):
+            valueDomain = utils.request_get(
+                auth=auth,
+                model="valuedomain",
+                name=data_element['data_element']['fields']['valueDomain']['fields']['name'],
+                app="aristotle_mdr"
+                )
             if not valueDomain:
-                valueDomain = utils.request_post(auth=auth,payload=data_element['data_element']['fields']['valueDomain'])
+                valueDomain = utils.request_post(
+                    auth=auth,
+                    payload=data_element['data_element']['fields']['valueDomain']
+                    )
             data_element['data_element']['fields']['valueDomain'] = valueDomain
-            distribution['fields']['data_elements'][i]['data_element'] = utils.request_post(auth=auth,payload=data_element['data_element'])
-        utils.request_post(auth=auth,payload=distribution)
+            distribution['fields']['data_elements'][i]['data_element'] = utils.request_post(
+                auth=auth,
+                payload=data_element['data_element']
+                )
+        utils.request_post(
+            auth=auth,
+            payload=distribution
+            )
+
 
 if __name__ == '__main__':
     execute_migration()
--- demo_meta_miner/miner.py
+++ demo_meta_miner/miner.py
@@ -1,20 +1,25 @@
 from sqlalchemy import create_engine
 import click
-# import yaml
-import json
-# import pprint
 import utils as utils
 # from demo_meta_miner.save_req import save_req
 
+
 @click.command()
 @click.option('--url', default='sqlite:///Test2.db', help='Full Database URl')
-@click.option('--auth', default='910923131171f6c4ae9bd84cbb5d5d44edb14436', help='Authentication token')
-
-def demo(url,auth):
-    """This script creates a data.json file, that contains all the database schema to be uploaded in Aristotle"""
+@click.option(
+    '--auth',
+    default='910923131171f6c4ae9bd84cbb5d5d44edb14436',
+    help='Authentication token'
+    )
+@click.option(
+    '--file',
+    default='data.json',
+    help='Spicify the json file name'
+    )
+def demo(url, auth, file):
+    """This script creates a data.json file,
+    that contains all the database schema to be uploaded in Aristotle"""
     engine = create_engine(url)
-    # from sqlalchemy.ext.automap import automap_base
-    # from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
     from sqlalchemy import MetaData
     metadata = MetaData()
 
@@ -22,24 +27,48 @@ def demo(url,auth):
 
     metadata.reflect(engine)
     # print(metadata.tables)
-    # save_req_object = save_req()
+
     table_data = {}
     distributions = []
-    dataset = utils.create_req(model="dataset",name= "testDatabase",app="aristotle_dse")
-    dataset = utils.request_post(auth=auth,payload=dataset)
+    dataset = utils.create_req(
+        model="dataset",
+        name="testDatabase",
+        app="aristotle_dse"
+        )
+    dataset = utils.request_post(auth=auth, payload=dataset)
     # import pdb; pdb.set_trace()
     for table in metadata.tables.keys():
-        extra_information_distribution = {"data_elements": [], "dataset": dataset}
+        extra_information_distribution = {
+            "data_elements": [],
+            "dataset": dataset
+            }
         table_data[table] = []
         for columns in metadata.tables[table].c:
-            value_domain = utils.create_req(model="valuedomain",name= columns.type.__repr__(),app="aristotle_mdr")
+            value_domain = utils.create_req(
+                model="valuedomain",
+                name=columns.type.__repr__(),
+                app="aristotle_mdr"
+                )
             extra_information_dataelement = {"valueDomain": value_domain}
-            data_element = utils.create_req(model="dataelement",name= columns.name.__repr__(),app="aristotle_mdr", other_field_data=extra_information_dataelement)
-            extra_information_distribution['data_elements'].append({'data_element' : data_element, "logical_path": table+"."+columns.name.__repr__()})
-        distribution = utils.create_req(model="distribution",name= table,app="aristotle_dse",other_field_data=extra_information_distribution)
+            data_element = utils.create_req(
+                model="dataelement",
+                name=columns.name.__repr__(),
+                app="aristotle_mdr",
+                other_field_data=extra_information_dataelement
+                )
+            extra_information_distribution['data_elements'].append({
+                'data_element': data_element,
+                "logical_path": table+"."+columns.name.__repr__()
+                })
+        distribution = utils.create_req(
+            model="distribution",
+            name=table, app="aristotle_dse",
+            other_field_data=extra_information_distribution
+            )
         distributions.append(distribution)
-    utils.save_req_file(distributions)
+    utils.save_req_file(distributions, file)
     conn.close()
+
 
 if __name__ == '__main__':
     demo()
