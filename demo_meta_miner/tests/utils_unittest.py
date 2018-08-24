b23b2aed9bb8d710c73ab528f65b051c818a38c7
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-08-24 14:28:43 +1000

    added unit tests utility functions

---
+++ demo_meta_miner/tests/utils_unittest.py
@@ -0,0 +1,46 @@
+import unittest
+import os
+import sys 
+sys.path.append('..')
+import utils
+import json
+
+
+class TestUtilsPy(unittest.TestCase):
+
+    def test_create_req(self):
+        result = utils.create_req(
+            model="test_dataelement",
+            name="Test1",
+            app="aristotle_mdr",
+            other_field_data={
+                "test_field1":"test_data1",
+                "test_field2":"test_data2"
+                }
+            )
+        payload = {
+            "concept_type": {
+                "app": "aristotle_mdr",
+                "model": "test_dataelement"
+            },
+            "fields": {
+                "name": "Test1",
+                "definition": "Placeholder",
+                "test_field1":"test_data1",
+                "test_field2":"test_data2",
+            }
+        }
+        self.assertEqual(payload, result)
+
+    def test_save_req_file(self):
+        file_name = "test.json"
+        data = {"test_data":"test_value"}
+        utils.save_req_file(data,file_name)
+        with open(file_name) as f:
+            read_data = f.read()
+        os.remove(file_name)
+        self.assertEqual(json.loads(read_data),data)
+
+
+if __name__ == '__main__':
+    unittest.main()
