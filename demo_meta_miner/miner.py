46defd1755c1eea95524e30142d3d3709e7a8ef7
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-08-27 12:09:36 +1000

    added changes for database name and enum values

--- demo_meta_miner/miner.py
+++ demo_meta_miner/miner.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 import click
 import utils as utils
 # from demo_meta_miner.save_req import save_req
@@ -6,6 +6,11 @@ import utils as utils
 
 @click.command()
 @click.option('--url', default='sqlite:///Test2.db', help='Full Database URl')
+@click.option(
+    '--database',
+    default='testDatabase1',
+    help='Database name on aristotle'
+    )
 @click.option(
     '--auth',
     default='910923131171f6c4ae9bd84cbb5d5d44edb14436',
@@ -16,11 +21,10 @@ import utils as utils
     default='data.json',
     help='Spicify the json file name'
     )
-def demo(url, auth, file):
+def demo(url, database, auth, file):
     """This script creates a data.json file,
     that contains all the database schema to be uploaded in Aristotle"""
     engine = create_engine(url)
-    from sqlalchemy import MetaData
     metadata = MetaData()
 
     conn = engine.connect()
@@ -32,33 +36,44 @@ def demo(url, auth, file):
     distributions = []
     dataset = utils.create_req(
         model="dataset",
-        name="testDatabase",
+        name=database,
         app="aristotle_dse"
         )
     dataset = utils.request_post(auth=auth, payload=dataset)
     # import pdb; pdb.set_trace()
-    for table in metadata.tables.keys():
+    for table_object in metadata.sorted_tables:
+        table = table_object.name
         extra_information_distribution = {
             "data_elements": [],
             "dataset": dataset
             }
         table_data[table] = []
         for columns in metadata.tables[table].c:
+            column_type = str(columns.type)
+            extra_information_value_domain = {}
+            if 'enum' in column_type.lower():
+                enum_types = column_type.replace("'","").split('(')[1].strip(')').split(',')
+                extra_information_value_domain['permissible_values'] = []
+                for index,enum_type in enumerate(enum_types):
+                    extra_information_value_domain['permissible_values'].append({"value": enum_type,
+                "meaning": "placeholder", "order":index})
+                
             value_domain = utils.create_req(
                 model="valuedomain",
-                name=columns.type.__repr__(),
-                app="aristotle_mdr"
+                name=column_type,
+                app="aristotle_mdr",
+                other_field_data=extra_information_value_domain
                 )
             extra_information_dataelement = {"valueDomain": value_domain}
             data_element = utils.create_req(
                 model="dataelement",
-                name=columns.name.__repr__(),
+                name=str(columns.name),
                 app="aristotle_mdr",
                 other_field_data=extra_information_dataelement
                 )
             extra_information_distribution['data_elements'].append({
                 'data_element': data_element,
-                "logical_path": table+"."+columns.name.__repr__()
+                "logical_path": table+"."+str(columns.name)
                 })
         distribution = utils.create_req(
             model="distribution",
