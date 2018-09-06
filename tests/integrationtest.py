ce69d12afdde99f7ebbbb6e0d95b6af04a026a5d
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-09-06 16:23:27 +1000

    changed some settings for integration testing

--- tests/integrationtest.py
+++ tests/integrationtest.py
@@ -6,12 +6,6 @@ import json
 import responses
 import click
 import sys
-import os
-
-
-if __name__ == '__main__':
-    TestUtilsPy.auth = os.environ.get('test_auth', TestUtilsPy.auth) 
-    unittest.main()
 
 class TestUtilsPy(unittest.TestCase):
     auth = "testAuth"
@@ -25,6 +19,7 @@ class TestUtilsPy(unittest.TestCase):
             )
         self.assertEqual({'detail': 'Invalid token.'},result.json())
     def test_request_get_no_result_case2(self):
+        self.auth = os.environ.get('test_auth', self.auth) 
         print(self.auth)
         result = utils.request_get(
             self.auth,
@@ -33,3 +28,7 @@ class TestUtilsPy(unittest.TestCase):
             url = 'http://0.0.0.0:8080'
             )
         self.assertEqual({'count': 0, 'next': None, 'previous': None, 'results': []},result.json())
+
+if __name__ == '__main__':
+    TestUtilsPy.auth = os.environ.get('test_auth', TestUtilsPy.auth) 
+    unittest.main()
