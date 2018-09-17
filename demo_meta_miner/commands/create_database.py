b14d2f491c2b38363f4fa4d954448a98972358b2
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-09-17 14:51:03 +1000

    made the create database tool more modular

--- demo_meta_miner/commands/create_database.py
+++ demo_meta_miner/commands/create_database.py
@@ -25,10 +25,38 @@ import requests
     help='Specify the aristotle url'
     )
 def create_database(dssuuid, dbtype, aristotleurl):
-    db_uri = 'sqlite:///Test2.db'
-    engine = create_engine(db_uri)
-    meta = MetaData(engine)
+    column_definitions = []
+    column_names = []
+    meta = MetaData()
+    response = get_dss_data(aristotleurl,dssuuid)
+    if response.status_code != 200:
+        print(
+            "Given {} aristotleurl does not contain {} dssuuid"
+            .format(aristotleurl, dssuuid))
+        return
+    data_json = response.json()
+    dss = data_json['data']['datasetSpecifications']['edges'][0]['node']
+    for data_element in dss['dssdeinclusionSet']:
+        column_name = create_column_name(
+            data_element['dataElement']['name'],
+            column_names
+            )
+        column_names.append(column_name)
+        value_domain = get_value_domain(data_element)
+        column_definitions.append(Column(column_name, value_domain))
+    table_name = dss['name'][:60]
+    t1 = Table(table_name, meta)
+    for column_definition in column_definitions:
+        t1.append_column(column_definition)
+    t1.append_column(Column('qwerty', Integer, primary_key=True))
+    try:
+        print(CreateTable(t1).compile(dialect=eval(dbtype).dialect()))
+    except NameError as err:
+            print("dbtype "+str(err))
+            return
 
+
+def get_dss_data(aristotleurl,dssuuid):
     grapgqlurl = aristotleurl+'/api/graphql/api'
     query = 'query {datasetSpecifications (uuid: "' + dssuuid + \
         '") {edges {node {name,dssclusterinclusionSet {dss {name}}' \
@@ -42,43 +70,28 @@ def create_database(dssuuid, dbtype, aristotleurl):
         grapgqlurl,
         params=(payload)
         )
-    if response.status_code != 200:
-        print(
-            "Given {} aristotleurl does not contain {} dssuuid"
-            .format(aristotleurl, dssuuid))
-        return
-    data_json = response.json()
-    column_definitions = []
-    column_names = []
-    dss = data_json['data']['datasetSpecifications']['edges'][0]['node']
-    for data_element in dss['dssdeinclusionSet']:
-        column_name = re.sub(
-            '[^a-zA-Z0-9\n\.]', '_', data_element['dataElement']['name']
-            ).replace('__', '_')[:60].upper()
-        i = 1
-        while column_name in column_names:
-            column_name = column_name[:60] + "_{}".format(i)
-            i = i+1
-        column_names.append(column_name)
-        value_domain = PlaceholderType()
-        if data_element['dataElement']['valueDomain']:
-            data_value_domain = data_element['dataElement']['valueDomain']
-            if 'Date' in data_value_domain['dataType']['name']:
-                value_domain = Date
-            if 'Number' in data_value_domain['dataType']['name']:
-                value_domain = Integer
-        column_definitions.append(Column(column_name, value_domain))
-    table_name = dss['name'][:60]
-    t1 = Table(table_name, meta)
-    for column_definition in column_definitions:
-        t1.append_column(column_definition)
-    t1.append_column(Column('qwerty', Integer, primary_key=True))
+    return response
 
-    try:
-        print(CreateTable(t1).compile(dialect=eval(dbtype).dialect()))
-    except NameError as err:
-            print("dbtype "+str(err))
-            return
+def create_column_name(data_element_name, column_names):
+    column_name = re.sub(
+            '[^a-zA-Z0-9\n\.]', '_', data_element_name
+            ).replace('__', '_')[:60].upper()
+    i = 1
+    while column_name in column_names:
+        column_name = column_name[:60] + "_{}".format(i)
+        i = i+1   
+    return column_name
+
+
+def get_value_domain(data_element):
+    value_domain = PlaceholderType()
+    if data_element['dataElement']['valueDomain']:
+        data_value_domain = data_element['dataElement']['valueDomain']
+        if 'Date' in data_value_domain['dataType']['name']:
+            value_domain = Date
+        if 'Number' in data_value_domain['dataType']['name']:
+            value_domain = Integer
+    return value_domain
 
 
 if __name__ == '__main__':
